4b0b88d78dd16c72c16c6ea78676d9bfcf16c5d0
Dan Badham <dan.badham@forgerock.com>
2023-03-27 15:04:31 +0100

    786: Adding create-config-archive.sh
    The helper script can be used to produce an archive that makes it easy to deploy the config to a tenant by extracting it in the git repo directory of that tenant.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/786

---
+++ scripts/create-config-archive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Helper script which produces a tar.gz containing the config
+#
+set -e
+cd "$(dirname "$0")"
+
+commit_hash=$(git rev-parse --short HEAD)
+
+cd sapig-overlay/
+file_name="../sapig-config-overlay-${commit_hash}.tar.gz"
+
+# Contains the config without the sapig-overlay parent dir
+tar -czf "${file_name}" ./*
+
+echo "Config archive written to: $(realpath "${file_name}")"
