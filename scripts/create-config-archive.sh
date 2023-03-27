eee6e5c15cef839a516fc9b58de9938b6c67f923
Dan Badham <dan.badham@forgerock.com>
2023-03-27 15:14:00 +0100

    786: Fixing script to work with updated directory
    https://github.com/SecureApiGateway/SecureApiGateway/issues/786

--- scripts/create-config-archive.sh
+++ scripts/create-config-archive.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 
 commit_hash=$(git rev-parse --short HEAD)
 
-cd sapig-overlay/
+cd ../sapig-overlay/
 file_name="../sapig-config-overlay-${commit_hash}.tar.gz"
 
 # Contains the config without the sapig-overlay parent dir
