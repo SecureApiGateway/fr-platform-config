ca49f53e9dc3d9a38a3810347a89c400d57fbe2d
shaunharrisonFR <115990205+shaunharrisonFR@users.noreply.github.com>
2024-03-05 10:48:51 +0000

    1307: Rename existing environments to [env]-ob (#33)

--- scripts/create-config-archive.sh
+++ scripts/create-config-archive.sh
@@ -4,11 +4,17 @@
 #
 set -e
 cd "$(dirname "$0")"
+if [ -z "$1" ]; then
+  echo "Params missing, ensure SAPIG_TYPE is set"
+  exit 1
+else
+  sapigType=$1
+fi
 
 commit_hash=$(git rev-parse --short HEAD)
 
-cd ../sapig-overlay/
-file_name="../sapig-config-overlay-${commit_hash}.tar.gz"
+cd ../sapig-overlay/$sapigType
+file_name="../../sapig-config-overlay-${sapigType}-${commit_hash}.tar.gz"
 
 # Contains the config without the sapig-overlay parent dir
 tar -czf "${file_name}" ./*
