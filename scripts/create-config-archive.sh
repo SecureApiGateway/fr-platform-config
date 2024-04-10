b752556f6e6327d79ffaa04f68fc2e05b1a40349
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2024-04-10 14:53:31 +0100

    1300: Build config for a specific realm (#49)
    The "realmName" string is used as a placeholder in directory paths that are realm dependent.
    
    Example realm specific paths:
    - sapig-overlay/core/realms/realmName
    - sapig-overlay/core/service-objects/realmName_user
    
    create-config-archive.sh has been updated to take 2 args:
    - --type (mandatory) values at the time of writing can be either ob or core
    - --realm (defaults to alpha) the FIDC realm that the config will be deployed to
    
    The tar produced by the script will have replaced the placeholder with the actual realm to deploy to.
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1300

--- scripts/create-config-archive.sh
+++ scripts/create-config-archive.sh
@@ -4,19 +4,42 @@
 #
 set -e
 cd "$(dirname "$0")"
-if [ -z "$1" ]; then
-  echo "Params missing, ensure SAPIG_TYPE is set"
+
+# Create config for the alpha realm by default
+realm="alpha"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --type)
+      sapigType="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --realm)
+      realm="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)
+      echo "Unknown option - valid options are --type and --realm"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "${sapigType}" ]; then
+  echo "--type must be specified as either ob or core"
   exit 1
-else
-  sapigType=$1
 fi
 
 commit_hash=$(git rev-parse --short HEAD)
 
-cd ../sapig-overlay/$sapigType
-file_name="../../sapig-config-overlay-${sapigType}-${commit_hash}.tar.gz"
+echo "Creating config archive for SAPI-G type: ${sapigType} and FIDC realm: ${realm}"
+
+cd "../sapig-overlay/${sapigType}"
+file_name="../../sapig-config-overlay-${sapigType}-${realm}-${commit_hash}.tar.gz"
 
 # Contains the config without the sapig-overlay parent dir
-tar -czf "${file_name}" ./*
+# Transforms the realmName placeholder found in paths to the desired realm name e.g. alpha
+tar -czf "${file_name}" --transform "s/realmName\(.*\)/${realm}\1/"  ./*
 
 echo "Config archive written to: $(realpath "${file_name}")"
